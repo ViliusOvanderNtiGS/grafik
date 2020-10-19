e1da653d8d3137ae4b2b9a4657d89b9ece5f7a5f
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2020-10-19 09:41:15 +0200

    initial commit

---
+++ grafik/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Raylib_cs;
+
+namespace grafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Raylib.InitWindow(800, 600, "test");
+
+            float x = 0;
+            float y = 0;
+
+            while (!Raylib.WindowShouldClose())
+            {
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    x += 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    y += 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    x -= 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    y -= 0.1f;
+                }
+
+                Raylib.BeginDrawing();
+
+                // Color myNewColor = new Color(124, 255, 12, 255);
+
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawRectangle((int)x, (int)y, 50, 50, Color.PINK);
+                Raylib.DrawRectangle((int)x + 5, (int)y + 35, 40, 5, Color.BLACK);
+
+
+
+
+                Raylib.EndDrawing();
+            }
+
+        }
+    }
+}
